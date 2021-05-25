399f7663ee7437964148b6b1d5c89cb5c18b6b64
btmortondev <bmortonbusiness@gmail.com>
2021-05-25 18:27:26 -0400

    NavBar changes
    made some simple changes to the navbar and the styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,16 @@
 import logo from './img/btm-logo.gif';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+
 
 const Navbar = () => {
     return ( 
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/work">Projects</Link>
+            <Link to="/" className="navLink">Home</Link>
+            <Link to="/work" className="navLink">Projects</Link>
             <img src= { logo } alt="btm logo" />
-            <Link to="/about">About Me</Link>
-            <Link to="/contact">Contact</Link>
+            <Link to="/about" className="navLink">About Me</Link>
+            <Link to="/contact" className="navLink">Contact</Link>
         </nav>
      );
 }
