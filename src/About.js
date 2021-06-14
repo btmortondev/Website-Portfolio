1f50c4d0adf7a95427cc107d36ecb787da2eaad5
btmortondev <bmortonbusiness@gmail.com>
2021-06-14 15:22:11 -0400

    About page setup
    I started on setting up the content that will be going on the about page. I have 1/3 of the contact fold done with.

--- src/About.js
+++ src/About.js
@@ -4,8 +4,21 @@ import Resume from './Content/BTM-21-Resume.pdf';
 const About = () => {
     return ( 
         <div className="about-content">
-            <h1>About Me</h1>
-            <a href= { Resume } target="_blank" rel="noreferrer">Resume</a>
+            <h1 className="about-title" >About Me</h1>
+            <div className="about-first-fold" >
+                <ul className="about-contact" >
+                    <li className="about-contact-li" >
+
+                    </li>
+                    <li className="about-contact-li" >
+
+                    </li>
+                    <li  >
+                        Download my
+                        <a className="resume-link" href= { Resume } target="_blank" rel="noreferrer"> resume </a>
+                    </li>
+                </ul>
+            </div>
         </div>
      );
 }
