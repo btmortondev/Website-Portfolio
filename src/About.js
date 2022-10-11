2d986c7d7174dc65366460cf7013e1ae23a0a61a
btmortondev <bmortonbusiness@gmail.com>
2022-10-11 10:40:39 -0400

    About Page Update
    Updated the About Page so that it is now responsive in landscape mode. Now starting to update the work/project page of the site!

--- src/About.js
+++ src/About.js
@@ -8,118 +8,99 @@ const About = () => {
   return (
     <div className="about-content">
       <h1 className="about-title">About Me</h1>
-      <div className="about-first-fold">
-        <ul className="about-contact">
-          <li className="about-contact-li">
-            <a
-              href="https://github.com/btmortondev"
-              target="_blank"
-              rel="noreferrer"
-              className="github-link"
-            >
-              <GitHubIcon />
-            </a>
-          </li>
-          <li className="about-contact-li">
-            <a
-              href="https://www.linkedin.com/in/brandon-morton-551508215/"
-              target="_blank"
-              rel="noreferrer"
-              className="linkedin-link"
-            >
-              {" "}
-              <LinkedInIcon />{" "}
-            </a>
-          </li>
-          <li className="about-contact-li">
-            Download my resume
-            <a
-              href={Resume}
-              target="_blank"
-              rel="noreferrer"
-              className="resume-download-icon"
-            >
-              <GetAppIcon />{" "}
-            </a>
-          </li>
-        </ul>
-        <p className="about-description">
-          I am a software engineer who recently graduated college from
-          Youngstown State University. Frontend development is what I'm looking
-          to do for work and all of the skills and knowledge I have applicable
-          is self taught. I currently live in Youngstown, OH and would
-          eventually like to move away. Remote work or somewhere that is
-          reasonable to comute would be great!
-        </p>
-      </div>
-      <div className="about-second-fold">
-        <h2 className="interests-title"> Interests/Passions </h2>
-        <ul className="interests-ul">
-          <li className="interests-li">
-            Web Development,
-            <br />
-            Mainly frontend
-            <br />
-            Would be interested in learning backend eventually
-          </li>
-          <li className="interests-li">
-            Game Development,
-            <br />
-            Unity
-            <br />
-            2D and 3D Animation
-            <br />
-            AI Design
-          </li>
-        </ul>
+      <div className="about-body" >
+        <div className="about-first-fold">
+          <ul className="about-contact">
+            <li className="about-contact-li">
+              <a
+                href="https://github.com/btmortondev"
+                target="_blank"
+                rel="noreferrer"
+                className="github-link"
+              >
+                <GitHubIcon fontSize="large"/>
+              </a>
+            </li>
+            <li className="about-contact-li">
+              <a
+                href="https://www.linkedin.com/in/brandon-morton-551508215/"
+                target="_blank"
+                rel="noreferrer"
+                className="linkedin-link"
+              >
+                {" "}
+                <LinkedInIcon fontSize="large"/>{" "}
+              </a>
+            </li>
+            <li className="about-contact-li">
+              Download my resume
+              <a
+                href={Resume}
+                target="_blank"
+                rel="noreferrer"
+                className="resume-download-icon"
+              >
+                <GetAppIcon fontSize="large"/>{" "}
+              </a>
+            </li>
+          </ul>
+          <p className="about-description">
+            I am a software engineer who recently graduated college from
+            Youngstown State University. Frontend development is what I'm currently
+            looking to do for work. I live in Youngstown, OH and would
+            eventually like to move away. I'm open to all opportunites 
+            including: remote work, close commute, or relocation depending on location
+            and opportunity!
+          </p>
+        </div>
 
-        <h2 className="skills-title"> Tools/Skills </h2>
-        <ul className="skills-ul">
-          <li className="skills-li">
-            React/Redux
-            <br />
-            JavaScript
-            <br />
-            HTML
-            <br />
-            CSS
-            <br />
-            Firebase
-            <br />
-            Drupal
-          </li>
-          <li className="skills-li">
-            Java
-            <br />
-            Visual Basic and .NET Framework
-            <br />
-            C++
-            <br />
-            Python
-            <br />
-            MySQL
-          </li>
-          <li className="skills-li">
-            C#
-            <br />
-            Unity
-            <br />
-            Pixel Art and Animations
-            <br />
-          </li>
-        </ul>
+        <div className="about-second-fold">
+          <h2 className="interests-title"> Interests/Passions </h2>
+          <ul className="interests-ul">
+            <li className="interests-li">
+              <p className="interests-items" > Web Development, </p>
+              <p className="interests-items" > Mainly frontend </p>
+              <p className="interests-items" > Would be interested in learning backend eventually </p>
+            </li>
+            <li className="interests-li">
+              <p className="interests-items" > Game Development, </p>
+              <p className="interests-items" > Unity </p>
+              <p className="interests-items" > 2D and 3D Animation </p>
+              <p className="interests-items" > AI Design </p>
+            </li>
+          </ul>
 
-        <h2 className="work-experience-title">Applicable Work Experience</h2>
-        <ul className="work-experience-ul">
-          <li className="work-experience-li">
-            Student Web Developer
-            <br />
-            @ Youngstown State University
-            <br />
-            March 2021 - May 2021
-            <br />
-          </li>
-        </ul>
+          <h2 className="skills-title"> Tools/Skills </h2>
+          <ul className="skills-ul">
+            <li className="skills-li">
+              <p className="skills-items" > React/Redux </p>
+              <p className="skills-items" > JavaScript </p>
+              <p className="skills-items" > HTML </p>
+              <p className="skills-items" > CSS </p>
+              <p className="skills-items" > Firebase </p>
+              <p className="skills-items" > Drupal </p>
+            </li>
+            <li className="skills-li">
+              <p className="skills-items" > Java </p>
+              <p className="skills-items" > Visual Basic and .NET Framework </p>
+              <p className="skills-items" > C++ </p>
+              <p className="skills-items" > Python </p>
+              <p className="skills-items" > MySQL </p>
+            </li>
+          </ul>
+
+          <h2 className="work-experience-title">Applicable Work Experience</h2>
+          <ul className="work-experience-ul">
+            <li className="work-experience-li">
+              Student Web Developer
+              <br />
+              @ Youngstown State University
+              <br />
+              March 2021 - May 2021
+              <br />
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
